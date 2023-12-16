7e133700b2bc53a673bf7204caac1ebfdcc98ac1
Munkyfoot <wopas182@gmail.com>
2023-12-15 20:13:09 -0800

    Use threading and sentence splitting to improve audio playback.

--- cli.py
+++ cli.py
@@ -11,6 +11,14 @@ if __name__ == "__main__":
         for chunk in response:
             response_text += chunk
             print(chunk, end="", flush=True)
+            speak_split = "\n" if agent.speaking else "."
+            if len(response_text.split(speak_split)) > 1:
+                sentence = response_text.split(speak_split)[0] + speak_split
+                sentence = sentence.strip()
+                speak_thread = threading.Thread(target=agent.speak, args=(sentence,))
+                speak_thread.start()
+                response_text = response_text.split(speak_split)[1].strip()
         print()
-        speak_thread = threading.Thread(target=agent.speak, args=(response_text,))
-        speak_thread.start()
+        if response_text != "":
+            speak_thread = threading.Thread(target=agent.speak, args=(response_text,))
+            speak_thread.start()
